f6d253c27e37e71280c0ec29baf5027105e27fb5
Scriptize <dblaylock8915@gmail.com>
2025-02-06 09:05:42 -0500

    refactoring and tests
    * added lib file with logic and tests
    * updated name (lowercase)
    *refactored main code

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,26 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Error processing args: {err}");
+        process::exit(1);
+
+    });
     
-    println!("Searching for {}", config.query);
-    println!("Looking in {}", config.file_path);
-    // dbg!(args);
+    // println!("Searching for {}", config.query);
+    // println!("Looking in {}", config.file_path);
+    // // dbg!(args);
 
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query : String,
-    file_path : String
-}
-
-// fn parse_config(args: &[String]) -> Config {
-//     let query = args[1].clone();
-//     let file_path = args[2].clone();
-
-//     Config{query, file_path} 
-// }
-
-impl Config{
-    fn new(args: &[String]) -> Config{
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Config{query, file_path}
+    if let Err(e) = minigrep::run(config){
+        println!("Application Error {e}");
+        process::exit(1);
     }
+
+
 }
+
